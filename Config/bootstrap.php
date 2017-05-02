a11e6a76d17af6204b76ec5eeb8410f3209ad2e4
jakolab <alejo.jko@gmail.com>
2017-05-01 20:30:03 -0500

    Project initalize
    Added the gitignore, the .htaccess files, index and bootstraping to the
    project.

---
+++ Config/bootstrap.php
@@ -0,0 +1,3 @@
+<?php
+
+require "globals.php";
---
+++ Config/globals.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * This file contains all the constants needed in the application.
+ * @package Config
+ * @author Jorge Alejandro Quiroz Serna (Jako) <alejo.jko@gmail.com>
+ * @version 1.0.0
+ * @copyright (c) 2017, Jakolab
+ */
+# This defines a shortcut to DIRECTORY_SEPARATOR
+define("DS", DIRECTORY_SEPARATOR);
+# Root directory.
+define("ROOT_DIR", realpath(__DIR__ . '/../'));
+# App folder.
+define("APP_DIR", ROOT_DIR . DS . "APP");
+# Cache folder.
+define("CACHE_DIR", ROOT_DIR . DS . "Cache");
+# Config folder.
+define("CONFIG_DIR", ROOT_DIR . DS . "Config");
+# Models folder into application.
+define("MODELS_DIR", APP_DIR . DS . "Models");
+# Framework folder.
+define("KERNEL_DIR", ROOT_DIR . DS . "GF");
+# Public folder.
+define("PUBLIC_DIR", ROOT_DIR . DS . "Public");
+# The vendors folder
+define("VENDOR_DIR", ROOT_DIR . DS . "vendor");
