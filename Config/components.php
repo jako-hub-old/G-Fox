dded3400c2964f51629327d7cf550c15e090ec2d
jakolab <alejo.jko@gmail.com>
2017-06-04 22:20:02 -0500

    Added components configuration file
    This file allows to define the components to be loaded by the
    application.

---
+++ Config/components.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * This file contains the application components to be loaded.
+ * @package Configs
+ * @author Jorge Alejandro Quiroz Serna <alejo.jko@gmail.com>
+ * @version 1.0
+ * @copyright (c) 2017, Jakolab
+ */
+return [
+    'Server' => \GF\Components\Web\Server::class,
+    'Session' => \GF\Components\Web\Session::class,
+];
