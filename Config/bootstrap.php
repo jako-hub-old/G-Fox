3363772fa808bf17492961dd7a384518caed01f8
jakolab <alejo.jko@gmail.com>
2017-06-04 22:20:25 -0500

    Updated boostrap.
    Added comments to the boostrap file.

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -1,3 +1,11 @@
 <?php
+/**
+ * This file includes the application bootstrap files.
+ *
+ * @package Configs
+ * @author Jorge Alejandro Quiroz Serna <alejo.jko@gmail.com>
+ * @version 1.0
+ * @copyright (c) 2017, Jakolab
+ */
 
 require "globals.php";
